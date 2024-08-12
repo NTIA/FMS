927c7188a5af2cb77694cfe477e767d03953db5d
Jaden Pieper <jpieper@ntia.gov>
2024-08-12 13:29:32 -0600

    Changes to matlab code to extend the low frequency response

--- matlab/FMS.m
+++ matlab/FMS.m
@@ -2,34 +2,36 @@ function [PsiM, PsiP] = FMS(audioFilename)
 % Creates fixed-size modulation spectrum (FMS) as described in
 % Voran, S. and Pieper, J. "A Powerful, Fixed-Size Modulation 
 % Spectrum Representation for Perceptually-Consistent Speech Evaluation."
-%
+% NTIA Tech Memo 24-57*
 % Usage: [PsiM, PsiP] = FMS(audioFilename)
 %
-% audioFilename is expected to be a .wav file with fs = 16, 32, 48, 22.05
-% or 44.1k.  Duration needs to be at least 60 ms. If file has more than
-% one channel, then channel 1 is used.
+% audioFilename is expected to be a .wav file with fs = 16, 24, 32, 48,
+% 22.05 or 44.1k.  Three seconds of audio are needed so files with duration
+% less than three seconds will be zero padded to 3 seconds. If file has
+% more than one channel, then channel 1 is used.
 %
-% PsiM and PsiP are matrices with size Nmel by 8. They contain the
-% magnitude and phase FMS, respectively, as given in Eqn. (6).
+% PsiM and PsiP are matrices with size Nmel by 11. They contain the
+% magnitude and phase FMS, respectively, as given in Eqn. (2).
 % Nmel depends on fs:
 %  fs      Nmel
 %-------   ----
 % 16k       32
+% 24k       36
 % 32k       40
 % 48k       45
 % 22.05k    35 
 % 44.1k     44
 %
-% For all 5 sample rates, the lower 32 mel bands match exactly. The 
+% For all 6 sample rates, the lower 32 mel bands match exactly. The 
 % bands above 32 cover the range from 8 kHz up to near the Nyquist
 % frequency for the given sample rate.
 %
-% Written January 12, 2023 by S. Voran at Institute for Telecommunication
+% Written April 24, 2024 by S. Voran at Institute for Telecommunication
 % Sciences in Boulder, Colorado, United States: svoran@ntia.gov
 %
 % Written and tested using MATLAB Version: 9.11.0.1809720 (R2021b) Update 1
 %
-% Note that Matlab uses one-based indexing, while the papers use
+% Note that Matlab uses one-based indexing, while the Tech. Memo uses
 % zero-based indexing
 
 %Check that .wav file has been specified
@@ -38,15 +40,16 @@ if ~strcmp(ext,'.wav')
     error('audioFilename is expected to name a .wav file')
 end
 
-
 [x,fs] = audioread(audioFilename); %read audio samples and sample rate
 x = x(:,1); %extract channel 1
 
 %Check for sufficient audio duration 
 
-if length(x)/fs < .060
-    error(['File contains less than 60 ms of audio which is not ' ...
-        'suitable for FMS'])
+if length(x)/fs < 3
+    warning(['Audio file is less than 3 sec. long, signal will ' ...
+        'be zero padded to 3.0 sec.'])
+    nShort = length(x) - 3*fs;  %Number of zeros needed
+    x = [x;zeros(nShort,1)];    %Add needed zeros
 end
 
 [Nw, Ns, Nmel, fu] = getConstants(fs);
@@ -61,11 +64,11 @@ Nf = floor( (Na-Nw)/Ns )+1;   %Calc. number of frames those samples allow
 Nt = 2*Nw;                  %Set number of samples in DFT
 
 %Make matrix Theta which implements filterbank that creates mel spectrum
-Theta = makeTheta(fs, Nt, Nmel, fu);
+Theta = makeTheta(fs, Nt, Nmel, fu); %Appendix A
 
 %Make matrix Phi which implements filterbank that creates modulation
 %spectrum
-Phi = makePhi(fs, Ns, Nf);
+Phi = makePhi(fs, Ns, Nf); %Appendix B
 
 %Generate normalized periodic Hamming Window with Nw samples - Eqn. (1)
 hammingWindow = ( 0.54-0.46*cos( 2*pi*[0:Nw-1]/Nw ) )/(0.54*Nw);
@@ -112,21 +115,24 @@ function [Nw, Ns, Nmel, fu] = getConstants(fs)
 %Select paramters based on sample rate (from Table I)
 if fs == 16000
     Nw = 256; %Number of samples per frame
-    Ns = 48;  %Stride in samples (advance Ns samples each frame) 
+    Ns = 32;  %Stride in samples (advance Ns samples each frame)
+elseif fs == 24000
+    Nw = 384;
+    Ns = 48;
 elseif fs == 32000
     Nw = 512;
-    Ns = 96;
+    Ns = 64;
 elseif fs == 48000
     Nw = 768;
-    Ns = 144;
+    Ns = 96;
 elseif fs == 22050
     Nw = 384;
-    Ns = 66;
+    Ns = 44;
 elseif fs == 44100
     Nw = 768;
-    Ns = 132;
+    Ns = 88;
 else
-    error(['Unexpected sample rate (16, 32, 48, 22.05, and ' ...
+    error(['Unexpected sample rate (16, 24, 32, 48, 22.05, and ' ...
         '44.1k are expected).'])
 end
 
@@ -159,27 +165,27 @@ function Theta = makeTheta(fs, Nt, Nmel, fu)
 %
 % Theta is NHertz by Nmel, where NHertz =  Nt/2 + 1;
 
-%Convert upper analysis limit from Hz to mel, FMS filter paper Eqn. (1)
+%Convert upper analysis limit from Hz to mel, Eqn. (14)
 fTildeU = 2595*log10(1+fu/700); 
 
-%Find mel interval, FMS filter paper Eqn. (2)
+%Find mel interval, Eqn. (15)
 deltaTilda = fTildeU/(Nmel+1);  %divid
 
 %Find band limits and centers in mel
 bTilda = deltaTilda*[0:Nmel+1];
 
-%Convert to Hz, FMS filter paper Eqn. (3)
+%Convert to Hz, Eqn. (16)
 b = 700*(10.^(bTilda/2595)-1);
 
-%Calculate DFT frequencies in Hz, FMS filter paper Eqn. (5)
+%Calculate DFT frequencies in Hz, Eqn. (18)
 f = [0:Nt/2]*fs/Nt;
 
-%Calculate filter normalizations, FMS filter paper Eqn. (6)
+%Calculate filter normalizations, Eqn. (19)
 eta = 1./( b(3:Nmel+2) - b(1:Nmel) );
 
 NHertz = Nt/2 + 1; %Number of DFT samples
 
-%Calculate filterbank, FMS filter paper Eqn. (4)
+%Calculate filterbank, Eqn. (17)
 Theta = zeros(NHertz,Nmel);
 for i = 0:Nmel-1 %Loop over bands
     for k = 0:NHertz-1 %Loop over DFT samples
@@ -203,58 +209,43 @@ function Phi = makePhi(fs, Ns, Nf)
 % Ns is stride of framing in samples (frame rate is fs/Ns frames per sec)
 % Nf is the number of frames to be processed
 %
-% Phi is N by Nmod, where N =  floor(Nf/2) + 1 and Nmod is 8
+% Phi is N by Nmod, where N =  floor(Nf/2) + 1 and Nmod is 11
 
 N = floor(Nf/2) + 1; %number of spectral samples available
-Nmod = 8; %set number of modulation spectrum samples
+Nmod = 11; %set number of modulation spectrum samples
 
-%Calculate log-scale frequency interval, FMS filter paper Eqn. (8)
-DeltaBar = ( log2(128) - log2(4) )/(Nmod - 1);
+%Calculate log-scale frequency interval, Eqn. (21)
+DeltaBar = ( log2(128) - log2(0.25) )/(Nmod - 2);
 
 %Calculate log-scale initial filter center frequencies, 
-%FMS filter paper Eqn. (9)
-bBar = log2(4) + [0:Nmod-1]*DeltaBar;
+%Eqn. (22)
+bBar = log2(.25) + [0:Nmod-2]*DeltaBar;
 
-%Calculate DFT bin spacing, FMS filter paper Eqn. (10)
+%Calculate thresholds, Eqn. (23)
+delta(1) = -inf;
+delta(2:Nmod-1) = (bBar(1:end-1) + bBar(2:end))/2;
+delta(Nmod) = inf;
+
+%Calculate DFT bin spacing, Eqn. (24)
 Deltah = fs/(Ns*Nf);
 
-%Calculate DFT bin log frequencies, FMS filter paper Eqn. (11)
+%Calculate DFT bin log frequencies, Eqn. (25)
 fBar = log2([0:N-1]*Deltah);
 
-%Adjust log-scale initial filter center frequencies to fall on DFT bins,
-%FMS filter paper Eqn. (12)
-bBarPrime = log2( round( ( 2.^bBar )/Deltah ) * Deltah );
-
-%Calculate filter half-widths, FMS filter paper Eqn. (13)
-deltaBar = DeltaBar/(2-sqrt(2));
-
 %Calculate filter weights to account for number of DFT samples spanned
-%by each filter, FMS filter paper Eqn. (15)
-nu = zeros(1,Nmod);
-for m = 0:Nmod-1
-    nu(m+1) = 1/sum( ( -deltaBar <= ( fBar - bBarPrime(m+1) ) ) & ...
-        ( ( fBar - bBarPrime(m+1) ) < deltaBar ) );
+%by each filter, Eqn. (28)
+nu = zeros(1,Nmod-1);
+for m = 1:Nmod-1
+    nu(m) = 1/sum( ( delta(m) < fBar ) & ...
+        ( fBar  <= delta(m+1) ) );
 end
 
-%Calculate filterbank, FMS filter paper Eqn. (14)
+%Calculate filterbank
 Phi = zeros(N,Nmod);
-for m = 0:Nmod - 1
-    for k = 0:N - 1
-        %lower slope
-        if ( bBarPrime(m+1)-deltaBar <= fBar(k+1) ) && ...
-                                ( fBar(k+1) < bBarPrime(m+1) )
-            Phi(k+1,m+1) = nu(m+1)*( fBar(k+1)- ...
-                                ( bBarPrime(m+1)-deltaBar ) )/deltaBar;
+Phi(1,1) = 1;         %Eqn. (26)
 
-        %upper slope    
-        elseif ( bBarPrime(m+1) <= fBar(k+1) ) && ...
-                        ( fBar(k+1) < bBarPrime(m+1)+deltaBar )
-            Phi(k+1,m+1) = nu(m+1)*...
-                        ( 1 - ( fBar(k+1)-bBarPrime(m+1) )/deltaBar );
-
-        %filter takes value 0    
-        else
-            Phi(k+1,m+1) = 0;
-        end
-    end
+%Eqn. (27)
+for m = 1:Nmod-1
+         indexList = find( ( delta(m) < fBar ) & ( fBar  <= delta(m+1) ) );
+         Phi(indexList,m+1) = nu(m);        
 end
